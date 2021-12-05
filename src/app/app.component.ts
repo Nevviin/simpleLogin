6b4fb0d967b4b6a9adb24009e7a853bb6361c0a7
Nevin <nevviin@gmail.com>
2021-12-05 21:56:59 +1100

    commit submit event handling for the login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from './services';
+import {FormGroup, FormBuilder,Validators, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleLogin';
+   form:FormGroup;
+  submitted = false;
+  constructor(
+    private loginService:LoginService,
+    private formBuilder:FormBuilder
+  ){}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+}
+get f() { return this.form.controls; }
+
+  onSubmit()
+  {
+    console.log("submit is called on button click");
+    console.log(this.f.username.value);
+    this.submitted=true;
+    this.loginService.Login(this.f.username.value,this.f.password.value);
+  }
 }
